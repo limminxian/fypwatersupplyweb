ff09419ba9178d4ccff1d40afc3e68e5a262bd6b
limminxian <limminxian316@gmail.com>
2023-02-01 14:04:04 +0800

    Update about.php

---
+++ about.php
@@ -0,0 +1,61 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Bootstrap Theme Simply Me</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+<div id="nav-placeholder">
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script>
+$(function(){
+  $("#nav-placeholder").load("navBarIndex.php");
+});
+</script>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  .bg-1 { 
+    background-color: #203354; /* White */
+    color: #555555;
+  }
+  .bg-2 { 
+    background-color: #474e5d; /* Dark Blue */
+    color: #ffffff;
+  }
+  .bg-3 { 
+    background-color: #fff; /* White */
+    color: #555555;
+  }
+  </style>
+</head>
+<body>
+
+
+
+<div class="container-fluid bg-1 text-center">
+  <h3>About Us</h3>
+  <img src="img/circle.jpg" class="img-circle" alt="Bird" width="350" height="350">
+  <h3>A local company that provides domestic water for citizens.</h3>
+</div>
+
+<div class="container-fluid bg-2 text-center">
+ <h3>FAQ</h3>
+  <h3>How to register?</h3>
+  <p>click "register" to come on with us</p>
+</div>
+
+<div class="container-fluid bg-3 text-center">
+  <h3>What are the services provided?</h3>
+  <p>add info</p>
+</div>
+
+</body>
+</html>
