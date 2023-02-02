766f426b0b712e960b3639a644ed2c84394a15b5
Ziaqufra <115613409+Ziaqufra@users.noreply.github.com>
2023-02-03 02:08:40 +0800

    Update editServiceDetails.php

--- editServiceDetails.php
+++ editServiceDetails.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
+<title>Edit service details</title>	
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link rel="stylesheet" href="style.css">
 <div id="nav-placeholder">
 </div>
-
+	
+<center>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -16,7 +18,11 @@ $(function(){
 });
 </script>
 </head>
+<h1>Edit Service Details</h1>	
 <body>
+<div class="center bg-img">
+
+<form action="" method="post" class="formcontainer">	
 <?php 
 include_once 'userClass.php';
 if(!isset($_SESSION['loginId'])){
@@ -45,7 +51,7 @@ else{
 		}
 	}?>
 	<br>
-				<input type="submit" name="submit" value="save" />
+				<input type="submit" class="formbutton"name="submit" value="save" />
 			</form>
 	<?php
 	if(isset($_POST["submit"])){
@@ -67,3 +73,4 @@ else{
    
 </body>
 </html> 
+	</center>
