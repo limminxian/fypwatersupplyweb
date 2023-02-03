ca546b1f6fcdc6d45b4c9104d97c231fa9aeecd7
Ziaqufra <115613409+Ziaqufra@users.noreply.github.com>
2023-02-04 01:25:26 +0800

    Update editHomeownerDetails.php

--- editHomeownerDetails.php
+++ editHomeownerDetails.php
@@ -7,6 +7,7 @@
 <div id="nav-placeholder">
 </div>
 
+<center>
 <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -16,7 +17,10 @@ $(function(){
 });
 </script>
 </head>
+	<h1>Edit Homeowner</h1>
 <body>
+<div class="center bg-img">	
+<form action="" method="post" class="formcontainer">	
 <?php 
 include_once 'userClass.php';
 if(!isset($_SESSION['loginId'])){
@@ -82,3 +86,4 @@ else{
    
 </body>
 </html> 
+	</center>
