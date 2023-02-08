65fc1302f1989387faaf816e492b7851a82cf6a6
Ziaqufra <115613409+Ziaqufra@users.noreply.github.com>
2023-02-08 14:47:34 +0800

    Update addEquipmentCSV.php

--- addEquipmentCSV.php
+++ addEquipmentCSV.php
@@ -9,8 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" crossorigin="anonymous"></script>
 </head>
-<body>
-<div class="center bg-img">	
+<body style="background-color:powderblue;">
 	
 <?php 
 include_once 'userClass.php';
