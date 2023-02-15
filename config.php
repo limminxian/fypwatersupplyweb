23fa118799e2e7386d6a0577076088ddc2aa0dc8
limminxian <limminxian316@gmail.com>
2023-02-15 15:20:16 +0800

    Update config.php

--- config.php
+++ config.php
@@ -5,21 +5,23 @@ $username = "bbd12ae4b2fcc3";
 $password = "df9ea7aa";
 $db = "heroku_80d6ea926f679b3";  */
 
-$servername = "ec2-3-225-213-67.compute-1.amazonaws.com";
+/* $servername = "ec2-3-225-213-67.compute-1.amazonaws.com";
 $username = "lbftgzbfbhpkxk";
 $password = "7730fd74a05533e54625120ba59d494a060111ce887ccd836c95a9d7494ed0b2";
 $db = "d1rhm1e7kg5b5e"; 
-$port = 5432;
+$port = 5432; */
 
 /* $servername = "localhost";
 $username = "root";
 $password = "";
 $db = "fyp"; */
 
+
 try {
    
-    $conn = mysqli_connect($servername,$username, $password, $db, $port);
+    //$conn = mysqli_connect($servername,$username, $password, $db, $port);
      //echo "Connected successfully"; 
+$conn = "dbname=d1rhm1e7kg5b5e host=ec2-3-225-213-67.compute-1.amazonaws.com port=5432 user=lbftgzbfbhpkxk password=7730fd74a05533e54625120ba59d494a060111ce887ccd836c95a9d7494ed0b2 sslmode=require";
     }
 catch(exception $e)
     {
