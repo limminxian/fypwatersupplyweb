42fa38abac3bf805447d3b3b3ce9515e6af3bf06
limminxian <limminxian316@gmail.com>
2023-02-15 18:49:41 +0800

    Update userClass.php

--- userClass.php
+++ userClass.php
@@ -90,8 +90,8 @@ class Service {
 			$this->$key = $value;
 		}
 		$conn = getdb();
-		$stmt = mysqli_prepare($conn,"INSERT INTO `SERVICETYPE` (`NAME`,`DESCRIPTION`,`TOTECH`,`CREATEDBY`) VALUES(?,?,?);");
-		mysqli_stmt_bind_param($stmt,"ssd", $this->name,$this->description,$_SESSION["loginId"]);
+		$stmt = mysqli_prepare($conn,"INSERT INTO `SERVICETYPE` (`NAME`,`DESCRIPTION`,`TOTECH`,`CREATEDBY`) VALUES(?,?,?,?);");
+		mysqli_stmt_bind_param($stmt,"ssd", $this->name,$this->description,$this->toTech,$_SESSION["loginId"]);
 		mysqli_stmt_execute($stmt);
 		if(mysqli_error($conn)!="" and !empty(mysqli_error($conn))){
 			$_SESSION["errorView"]=mysqli_error($c);
