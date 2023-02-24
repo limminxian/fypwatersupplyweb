ef88882de482a623e82acef3231f1f7d8a40deec
limminxian <limminxian316@gmail.com>
2023-02-25 00:03:10 +0800

    Update manageServiceCompanyadmin.php

--- manageServiceCompanyadmin.php
+++ manageServiceCompanyadmin.php
@@ -83,7 +83,7 @@ foreach($s as $r){
 				<button  value="<?=base64_encode(serialize($r))?>" class="edit"name="rates"/>rates</button>
 			</td>
 			<?php
-		if($r->createdby!=0){
+		if($r->createdby!=1){
 			
 			?>
 			<td>
